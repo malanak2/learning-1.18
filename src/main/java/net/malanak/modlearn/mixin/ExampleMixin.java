3c3b0f46dd6bfeedd4c4297d594ee671eaf74e5a
malanak <malanak@pop-os.localdomain>
2022-07-06 16:57:42 +0200

    Initial commit

---
+++ src/main/java/net/malanak/modlearn/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.malanak.modlearn.mixin;
+
+import net.malanak.modlearn.LearnMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		LearnMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
