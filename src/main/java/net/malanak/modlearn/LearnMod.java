c5ca09f8da3a696a8eefbcbd289c35bfeb9f6ff0
malanak2 <malanakgames@seznam.cz>
2022-07-06 18:25:46 +0200

    Added block and ore form for mythril

--- src/main/java/net/malanak/modlearn/LearnMod.java
+++ src/main/java/net/malanak/modlearn/LearnMod.java
@@ -1,6 +1,7 @@
 package net.malanak.modlearn;
 
 import net.fabricmc.api.ModInitializer;
+import net.malanak.modlearn.block.ModBlocks;
 import net.malanak.modlearn.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,6 @@ public class LearnMod implements ModInitializer {
 	public void onInitialize() {
 
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
